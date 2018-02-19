be160168678adae662cf9c4c80a2d636e9c7fcdd
Benjamin Wenzel <benwenzel@runtimeparadigm.com>
2018-02-18 21:52:22 -0500

    Finished implementing test cases, added two additional testcases for matching none of the criteria.

--- QuoteSearchTests.java
+++ QuoteSearchTests.java
@@ -23,7 +23,8 @@ public class QuoteSearchTests {
         //run search
         QuoteList searchResults = quotes.search("Richard Nixon", 0);
         
-        assertTrue(searchResults.getSize() == 1 && searchResults.getQuote(0).getQuoteText().equals("I know that you believe you understand what you think I said, but I am not sure you realize that what you heard is not what I meant."));
+        assertTrue(searchResults.getSize() == 1);
+        assertTrue(searchResults.getQuote(0).getQuoteText().equals("I know that you believe you understand what you think I said, but I am not sure you realize that what you heard is not what I meant."));
    }
     
         
@@ -36,39 +37,79 @@ public class QuoteSearchTests {
         //run search
         QuoteList searchResults = quotes.search("Eschew obfuscation!", 1);
         
-        assertTrue(searchResults.getSize() == 1 && searchResults.getQuote(0).getAuthor().equals("Don Cunningham"));
+        assertTrue(searchResults.getSize() == 1);
+        assertTrue(searchResults.getQuote(0).getAuthor().equals("Don Cunningham"));
    }
     
         
     @Test
     public void match_both_test() {
+        //load quote file and store quotes in a QuoteList
+        QuoteSaxParser qParser = new QuoteSaxParser("quotes.xml");
+        QuoteList quotes = qParser.getQuoteList();
         
-        //Create the quote List to search
-        QuoteList quotes = new QuoteList();
+        //run search
+        QuoteList searchResults = quotes.search("the", 2);
         
-        //Not sure how to put all the quotes in the quotelist here
         
-        QuoteList searchResults = quotes.search("test", 2);
+        System.out.println(searchResults.getQuote(0).getAuthor());
         
         //Need to figure out what we should do for assertion. Maybe Create a Quote object with the exact quote we want?
         
-        assertTrue(true);
-  
+        //check size
+        assertTrue(searchResults.getSize() == 4);
+        
+        //check quotes
+        assertTrue(searchResults.getQuote(0).getQuoteText().equals("For every problem there is one solution which is simple, neat, and wrong."));
+        assertTrue(searchResults.getQuote(1).getQuoteText().equals("The only man who behaves sensibly is my tailor; he takes my measure anew every time he sees me, whilst all the rest go on with their old measurements, expecting them to fit me."));
+        assertTrue(searchResults.getQuote(2).getQuoteText().equals("We often do not know how to make those decisions [about a system design] until we can play with the system."));
+        assertTrue(searchResults.getQuote(3).getQuoteText().equals("My life is has been one continual search for answers to one short question. The subject and the answer changes, but the question remains simply\" \"Why?\"."));
+        //check authors
+        assertTrue(searchResults.getQuote(0).getAuthor().equals("H. L. Mencken"));
+        assertTrue(searchResults.getQuote(1).getAuthor().equals("George Bernard Shaw"));
+        assertTrue(searchResults.getQuote(2).getAuthor().equals("David Parnas \"Software Aspects of SDI\""));
+        assertTrue(searchResults.getQuote(3).getAuthor().equals("The Anonymous Researcher"));
    }
     
     @Test
-    public void match_none_test() {
+    public void match_none_author_test() {
         
-        //Create the quote List to search
-        QuoteList quotes = new QuoteList();
+        //arrange
+        QuoteSaxParser qParser = new QuoteSaxParser("quotes.xml");
+        QuoteList quotes = qParser.getQuoteList();
         
-        //Not sure how to put all the quotes in the quotelist here
+        //act
+        QuoteList searchResults = quotes.search("nope", 0);
         
-        QuoteList searchResults = quotes.search("test", 0);
+        //assert
+        assertTrue(searchResults.getSize() == 0);
+   }
+
+    @Test
+    public void match_none_text_test() {
         
-        //Need to figure out what we should do for assertion. Maybe Create a Quote object with the exact quote we want?
+        //arrange
+        QuoteSaxParser qParser = new QuoteSaxParser("quotes.xml");
+        QuoteList quotes = qParser.getQuoteList();
         
-        assertTrue(true);
-  
+        //act
+        QuoteList searchResults = quotes.search("nope", 1);
+        
+        //assert
+        assertTrue(searchResults.getSize() == 0);
+   }
+
+    @Test
+    public void match_none_both_test() {
+        
+        //arrange
+        QuoteSaxParser qParser = new QuoteSaxParser("quotes.xml");
+        QuoteList quotes = qParser.getQuoteList();
+        
+        //act
+        QuoteList searchResults = quotes.search("nope", 2);
+        
+        //assert
+        assertTrue(searchResults.getSize() == 0);
    }
 }
